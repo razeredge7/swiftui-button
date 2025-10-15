52d58736defc8f2033f938bf4cdbc7ba51247e96
Kevin <ckevin2797@gmail.com>
2025-10-15 08:03:00 +0700

    Initial Commit

---
+++ SwiftUIButton/SwiftUIButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonApp.swift
+//  SwiftUIButton
+//
+//  Created by Kevin on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
