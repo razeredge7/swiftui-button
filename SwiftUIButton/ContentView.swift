dd750f7d1ec838cd19fb676c6ddf68cc7675d8ec
Kevin <ckevin2797@gmail.com>
2025-10-22 13:07:18 +0700

    Button Implementation

--- SwiftUIButton/ContentView.swift
+++ SwiftUIButton/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button{
+            
+        } label: {
+            Text("Hello World")
+                .background(.purple)
+                .foregroundStyle(.white)
+                .font(.title)
+                .padding()
+            
         }
-        .padding()
+//        .padding()
     }
 }
 
